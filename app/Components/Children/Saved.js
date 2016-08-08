c7df1e9f681fc3cd9d0150cf77704bc011adbb2c
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-08-08 15:16:57 -0400

    html and react components structure

---
+++ app/Components/Children/Saved.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var Saved = React.createClass({
+
+	render: function() {
+		
+		return (
+
+			<div className="container">
+
+				<div className="row">
+					<div className="col-md-12">
+						<div className="panel panel-default">
+							<div className="panel-heading">
+								<h2>Saved Articles</h2>
+							</div>
+							<div className="panel-body">
+								
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		)
+	}
+
+});
+
+module.exports = Saved;
